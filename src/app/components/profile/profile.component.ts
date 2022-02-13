21eeabbd1d0e16e9449c01f25646555183f27e3a
maxime <maxbln7513@gmail.com>
2022-02-13 17:53:23 +0100

    profil form

--- src/app/components/profile/profile.component.ts
+++ src/app/components/profile/profile.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { HotToastService } from '@ngneat/hot-toast';
 import { User } from 'firebase/auth';
 import { concatMap, switchMap } from 'rxjs';
@@ -18,15 +18,7 @@ import { UntilDestroy, untilDestroyed } from '@ngneat/until-destroy';
 export class ProfileComponent implements OnInit {
 
   user$ = this.usersService.currentUserProfile$;
-
-  profileForm = new FormGroup({
-    uid: new FormControl(''),
-    displayName: new FormControl(''),
-    firstName: new FormControl(''),
-    lastName: new FormControl(''),
-    phone: new FormControl(''),
-    address: new FormControl(''),
-  });
+  profileForm: FormGroup;
   
   constructor(private authService: AuthService, private imageUploadService: ImageUploadService, private usersService: UsersService, private toast: HotToastService,) { }
 
@@ -36,6 +28,26 @@ export class ProfileComponent implements OnInit {
       .subscribe((user) => {
         this.profileForm.patchValue({ ...user });
     });
+    
+    // init formulaire
+    this.validateform();
+  }
+
+  // init validator
+  validateform() {
+    this.profileForm = new FormGroup(
+      {
+        uid: new FormControl(''),
+        displayName: new FormControl("", Validators.required),
+        firstName: new FormControl(""),
+        lastName: new FormControl(''),
+        phone: new FormControl(''),
+      }
+    );
+  }
+
+  get displayName() {
+    return this.profileForm.get('displayName');
   }
 
   uploadImage(event: any, user: ProfileUser) {
@@ -55,14 +67,19 @@ export class ProfileComponent implements OnInit {
   }
 
   saveProfile() {
+    if (!this.profileForm.valid) {
+      console.log('formulaire invalid');
+      return;
+    }
+
     const profileData = this.profileForm.value;
     this.usersService
       .updateUser(profileData)
       .pipe(
         this.toast.observe({
-          loading: 'Saving profile data...',
-          success: 'Profile updated successfully',
-          error: 'There was an error in updating the profile',
+          loading: 'Modification des informations ...',
+          success: 'Mofidication du profil effectuée',
+          error: "Une erreur c'est produite",
         })
       )
       .subscribe();
