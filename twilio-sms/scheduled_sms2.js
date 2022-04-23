a40d347876b860ebd097b79696945d5996d45682
maxime <maxbln7513@gmail.com>
2022-04-23 18:11:48 +0200

    twillio account env

--- twilio-sms/scheduled_sms2.js
+++ twilio-sms/scheduled_sms2.js
@@ -7,8 +7,7 @@ const client = require('twilio')(accountSid, authToken);
 async function sendScheduledSms(date, phone, message) {
     const sms = await client.messages.create({
       body: message,
-      // messagingServiceSid: 'MG7647de62283c642f5ec58b3a95fdd080',      
-      messagingServiceSid: 'MGb01c041156f36c44e6c4281c2db58a4d',   
+      messagingServiceSid: 'MG7647de62283c642f5ec58b3a95fdd080',      
       to: phone,
       scheduleType: 'fixed',
       sendAt: date
